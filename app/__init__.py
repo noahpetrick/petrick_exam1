5a4f4d4dc433f01a9b8523a7850acffca98aae03
noah.petrick <noah.petrick@bobcats.gcsu.edu>
2024-09-11 13:28:39 -0400

    Complete assignment for Exam 1.

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,3 @@ app = create_app()
 app.secret_key = 'your-secret'  # Replace with an environment variable
 
 from . import routes
-
-
-
--- app/routes.py
+++ app/routes.py
@@ -9,3 +9,6 @@ def index():
 def about():
     return render_template('about.html')
 
+@app.route('/exam')
+def exam():
+    return render_template('exam.html')
